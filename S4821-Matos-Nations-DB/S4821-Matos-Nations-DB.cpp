ae9d9f175c2e3817c34757130393fb08a37c9fc5
VICTOR MATOS <vmatos@elcamino.edu>
2025-04-03 15:33:40 -0700

    Incomplete

--- S4821-Matos-Nations-DB/S4821-Matos-Nations-DB.cpp
+++ S4821-Matos-Nations-DB/S4821-Matos-Nations-DB.cpp
@@ -1,5 +1,6 @@
 // S4821-Matos-Nations-DB.cpp 
 
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -31,7 +32,27 @@ public:
     bool operator< (const Nation& other) const {
         return (this->country < other.country);
     }
+
+    friend ostream& operator<< (ostream& sout, const Nation& n) {
+        sout << " Country:  " << n.country << endl
+            << " Capital:  " << n.capital << endl
+            << " Currency: " << n.currency << endl
+            << " Language: " << n.language << endl;
+        return sout;
+    }
 };
+//------------------------------------------------------------------------
+template<typename T>
+void showSet(set<T> s, string caption = "") {
+    cout << endl << caption << endl;
+    for (T item : s) {
+        cout << item << endl;
+    }
+}
+
+
+
+
 // ----------------------------------------------------------------------
 void experiment0() {
     Nation n("Wakanda", "Wakanda City", "USD", "English; Spanish");
@@ -83,7 +104,7 @@ void experiment02(vector<Nation>& db)
     //    cout << "Found - pos " << pos << endl;
     //}
 
-    string lang = "Spanish";
+    string lang = "English";
     set<Nation> enNations;
     for (Nation n : db) {
         if (n.language.find(lang) != string::npos) {
@@ -93,12 +114,40 @@ void experiment02(vector<Nation>& db)
 
     int spy = 0;
 }
-
+//-------------------------------------------------------------
 //-------------------------------------------------------------
 void experiment03(vector<Nation> db) {
     //TODO - Transfer vector data into a set<Nation>
+    set<Nation> sdb;
+    for (auto n : db) {
+        sdb.insert(n);
+    }
+
+    //showSet(sdb, "Set - all nations");
 
     //Create a Map to index capital city
+    map<string, set<Nation>::iterator> mcapital;
+    for (auto it = sdb.begin(); it != sdb.end(); it++) {
+        Nation n = *it;
+        mcapital[n.capital] = it; 
+        //mcapital.insert(pair< string, set<Nation>::iterator>{n.capital, it});
+    }
+
+    //Test one entry from the map (Retrieve by capital )
+    auto it1 = mcapital.find("Rome");
+    if (it1 != mcapital.end()) {
+        cout << it1->second->country << endl;
+    }
+    else {
+        cout << "No country associated to that capital city \n";
+    }
+
+    //Retrieve using country name
+    auto it2 = sdb.find(Nation("Cuba"));
+    cout << "Success - Country search  " << it2->capital << endl;
+
+
+
 
     //Test retrieval using country name -or- capital city
 
